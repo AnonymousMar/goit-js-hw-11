5437868df2d3b1b9e24b27ab9d3d78f12fef3eca
AnonymousMar <119819802+AnonymousMar@users.noreply.github.com>
2023-06-23 16:26:25 +0200

    .

---
+++ src/js/index.js
@@ -0,0 +1,145 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const apiKey = '37745610-94fa81a6ea5e93adfdf3ac469';
+
+const searchForm = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+
+let page = 1;
+let currentQuery = '';
+let lightbox = null;
+let lastLoadedPage = 0;
+let isLastPageLoaded = false;
+
+searchForm.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const searchQuery = event.target.elements.searchQuery.value.trim();
+
+    if (searchQuery === '') {
+        return;
+    }
+
+    currentQuery = searchQuery;
+    page = 1;
+    lastLoadedPage = 0;
+    isLastPageLoaded = false;
+    clearGallery();
+    fetchImages(searchQuery, page);
+}
+
+function fetchImages(searchQuery, page) {
+    if (page <= lastLoadedPage || isLastPageLoaded) {
+        return;
+    }
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.hits.length === 0) {
+                showNoImagesMessage();
+            } else {
+                showTotalImagesCount(data.totalHits);
+                renderImages(data.hits);
+                initializeLightbox();
+                lastLoadedPage = page;
+                if (data.hits.length < 40) {
+                    hideLoadMoreButton();
+                    isLastPageLoaded = true;
+                } else {
+                    showLoadMoreObserver();
+                }
+            }
+        })
+        .catch(error => {
+            console.log('Error fetching images:', error);
+        });
+}
+
+function renderImages(images) {
+    const imageCards = images.map(image => createImageCard(image));
+    gallery.insertAdjacentHTML('beforeend', imageCards.join(''));
+}
+
+function createImageCard(image) {
+    return `
+    <a href="${image.largeImageURL}" class="photo-card">
+      <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+        <p class="info-item"><b>Views:</b> ${image.views}</p>
+        <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+        <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+      </div>
+    </a>
+  `;
+}
+
+function clearGallery() {
+    gallery.innerHTML = '';
+}
+
+function showNoImagesMessage() {
+    Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+    );
+}
+
+function showTotalImagesCount(count) {
+    Notiflix.Notify.success(`Hooray! We found ${count} images.`);
+}
+
+function initializeLightbox() {
+    if (lightbox) {
+        lightbox.destroy();
+    }
+    lightbox = new SimpleLightbox('.gallery a');
+    lightbox.refresh();
+}
+
+function hideLoadMoreButton() {
+    const loadMoreBtn = document.querySelector('.load-more');
+    loadMoreBtn.style.display = 'none';
+}
+
+function showLoadMoreObserver() {
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver(handleLoadMore, options);
+    const loadMoreTrigger = document.querySelector('.load-more');
+
+    observer.observe(loadMoreTrigger);
+}
+
+function handleLoadMore(entries, observer) {
+    if (entries[0].isIntersecting) {
+        observer.unobserve(entries[0].target);
+        page++;
+        fetchImages(currentQuery, page);
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+
+function handleScroll() {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+        page++;
+        fetchImages(currentQuery, page);
+    }
+}
